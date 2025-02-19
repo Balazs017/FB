ef6274223e7b4d19817b3655f9fb8908e3854f88
Rendszergazda <rendszergazda@blathy.info>
2025-02-19 08:49:57 +0100

    rendetlen

---
+++ ketdim/rajzol6.cs
@@ -0,0 +1,18 @@
+using System;
+
+class Rajzol2d{
+    static void Main(){
+        int meret = 8;
+        int[,] palya = new int[meret,meret];
+
+        for (int i = 0; i<meret;i++)
+        {
+            for(int j = 0; j< meret; j++)
+            {
+                
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
