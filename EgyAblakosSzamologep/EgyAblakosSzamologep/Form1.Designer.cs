28862765dc1438e1d6b0fa0827039f435944811d
Rendszergazda <rendszergazda@blathy.info>
2025-03-12 12:30:30 +0100

    Egyablakos majdnem kész

---
+++ EgyAblakosSzamologep/EgyAblakosSzamologep/Form1.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace KetAblakosSzamologep
+{
+    partial class Ketablakos
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            balSzam = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button23 = new Button();
+            button24 = new Button();
+            button25 = new Button();
+            button26 = new Button();
+            button28 = new Button();
+            button29 = new Button();
+            History = new ListBox();
+            button12 = new Button();
+            SuspendLayout();
+            // 
+            // balSzam
+            // 
+            balSzam.Enabled = false;
+            balSzam.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            balSzam.Location = new Point(78, 52);
+            balSzam.Name = "balSzam";
+            balSzam.Size = new Size(226, 33);
+            balSzam.TabIndex = 0;
+            balSzam.Text = "0";
+            balSzam.TextAlign = HorizontalAlignment.Right;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button1.Location = new Point(80, 111);
+            button1.Name = "button1";
+            button1.Size = new Size(50, 50);
+            button1.TabIndex = 2;
+            button1.Text = "7";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += BalSzamok;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button2.Location = new Point(153, 111);
+            button2.Name = "button2";
+            button2.Size = new Size(50, 50);
+            button2.TabIndex = 3;
+            button2.Text = "8";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += BalSzamok;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button3.Location = new Point(224, 111);
+            button3.Name = "button3";
+            button3.Size = new Size(50, 50);
+            button3.TabIndex = 4;
+            button3.Text = "9";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += BalSzamok;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button4.Location = new Point(224, 196);
+            button4.Name = "button4";
+            button4.Size = new Size(50, 50);
+            button4.TabIndex = 7;
+            button4.Text = "6";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += BalSzamok;
+            // 
+            // button5
+            // 
+            button5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button5.Location = new Point(153, 196);
+            button5.Name = "button5";
+            button5.Size = new Size(50, 50);
+            button5.TabIndex = 6;
+            button5.Text = "5";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += BalSzamok;
+            // 
+            // button6
+            // 
+            button6.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button6.Location = new Point(80, 196);
+            button6.Name = "button6";
+            button6.Size = new Size(50, 50);
+            button6.TabIndex = 5;
+            button6.Text = "4";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += BalSzamok;
+            // 
+            // button7
+            // 
+            button7.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button7.Location = new Point(224, 278);
+            button7.Name = "button7";
+            button7.Size = new Size(50, 50);
+            button7.TabIndex = 10;
+            button7.Text = "3";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += BalSzamok;
+            // 
+            // button8
+            // 
+            button8.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button8.Location = new Point(153, 278);
+            button8.Name = "button8";
+            button8.Size = new Size(50, 50);
+            button8.TabIndex = 9;
+            button8.Text = "2";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += BalSzamok;
+            // 
+            // button9
+            // 
+            button9.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button9.Location = new Point(80, 278);
+            button9.Name = "button9";
+            button9.Size = new Size(50, 50);
+            button9.TabIndex = 8;
+            button9.Text = "1";
+            button9.UseVisualStyleBackColor = true;
+            button9.Click += BalSzamok;
+            // 
+            // button10
+            // 
+            button10.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button10.Location = new Point(80, 352);
+            button10.Name = "button10";
+            button10.Size = new Size(123, 50);
+            button10.TabIndex = 11;
+            button10.Text = "0";
+            button10.UseVisualStyleBackColor = true;
+            button10.Click += BalSzamok;
+            // 
+            // button11
+            // 
+            button11.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button11.ForeColor = Color.Red;
+            button11.Location = new Point(224, 352);
+            button11.Name = "button11";
+            button11.Size = new Size(50, 50);
+            button11.TabIndex = 12;
+            button11.Text = "Del";
+            button11.UseVisualStyleBackColor = true;
+            button11.Click += button11_Click;
+            // 
+            // button23
+            // 
+            button23.BackColor = Color.Lime;
+            button23.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button23.ForeColor = SystemColors.ControlText;
+            button23.Location = new Point(419, 139);
+            button23.Name = "button23";
+            button23.Size = new Size(50, 50);
+            button23.TabIndex = 24;
+            button23.Text = "+";
+            button23.UseVisualStyleBackColor = false;
+            // 
+            // button24
+            // 
+            button24.BackColor = Color.Lime;
+            button24.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button24.ForeColor = SystemColors.ControlText;
+            button24.Location = new Point(558, 139);
+            button24.Name = "button24";
+            button24.Size = new Size(50, 50);
+            button24.TabIndex = 25;
+            button24.Text = "-";
+            button24.UseVisualStyleBackColor = false;
+            // 
+            // button25
+            // 
+            button25.BackColor = Color.Lime;
+            button25.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button25.ForeColor = SystemColors.ControlText;
+            button25.Location = new Point(419, 231);
+            button25.Name = "button25";
+            button25.Size = new Size(50, 50);
+            button25.TabIndex = 26;
+            button25.Text = "*";
+            button25.UseVisualStyleBackColor = false;
+            button25.Click += Muveletklikk;
+            // 
+            // button26
+            // 
+            button26.BackColor = Color.Lime;
+            button26.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button26.ForeColor = SystemColors.ControlText;
+            button26.Location = new Point(558, 231);
+            button26.Name = "button26";
+            button26.Size = new Size(50, 50);
+            button26.TabIndex = 27;
+            button26.Text = "/";
+            button26.UseVisualStyleBackColor = false;
+            button26.Click += Muveletklikk;
+            // 
+            // button28
+            // 
+            button28.BackColor = Color.Lime;
+            button28.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button28.ForeColor = SystemColors.ControlText;
+            button28.Location = new Point(419, 139);
+            button28.Name = "button28";
+            button28.Size = new Size(50, 50);
+            button28.TabIndex = 24;
+            button28.Text = "+";
+            button28.UseVisualStyleBackColor = false;
+            button28.Click += Muveletklikk;
+            // 
+            // button29
+            // 
+            button29.BackColor = Color.Lime;
+            button29.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button29.ForeColor = SystemColors.ControlText;
+            button29.Location = new Point(558, 139);
+            button29.Name = "button29";
+            button29.Size = new Size(50, 50);
+            button29.TabIndex = 25;
+            button29.Text = "-";
+            button29.UseVisualStyleBackColor = false;
+            button29.Click += Muveletklikk;
+            // 
+            // History
+            // 
+            History.FormattingEnabled = true;
+            History.ItemHeight = 15;
+            History.Location = new Point(660, 52);
+            History.Name = "History";
+            History.Size = new Size(236, 439);
+            History.TabIndex = 28;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.Lime;
+            button12.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 238);
+            button12.ForeColor = SystemColors.ControlText;
+            button12.Location = new Point(419, 297);
+            button12.Name = "button12";
+            button12.Size = new Size(189, 50);
+            button12.TabIndex = 29;
+            button12.Text = "=";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += Muveletklikk;
+            // 
+            // Ketablakos
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1193, 619);
+            Controls.Add(button12);
+            Controls.Add(History);
+            Controls.Add(button26);
+            Controls.Add(button25);
+            Controls.Add(button29);
+            Controls.Add(button24);
+            Controls.Add(button28);
+            Controls.Add(button23);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button9);
+            Controls.Add(button4);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(balSzam);
+            Name = "Ketablakos";
+            Text = "Számológép";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox balSzam;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button23;
+        private Button button24;
+        private Button button25;
+        private Button button26;
+        private Button button28;
+        private Button button29;
+        private ListBox History;
+        private Button button12;
+    }
+}
---
+++ EgyAblakosSzamologep/EgyAblakosSzamologep/Form1.cs
@@ -0,0 +1,107 @@
+namespace KetAblakosSzamologep
+{
+    public partial class Ketablakos : Form
+    {
+        int bal, jobb;
+        public Ketablakos()
+        {
+            InitializeComponent();
+        }
+
+        private void BalSzamok(object sender, EventArgs e)
+        {
+            char be = (sender as Button).Text[0];
+
+            if (balSzam.Text == "0")
+            {
+                balSzam.Text = be.ToString();
+
+            }
+            else
+            {
+                balSzam.Text += be.ToString();
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            balSzam.Text = balSzam.Text.Substring(0, balSzam.Text.Length - 1);
+            if (balSzam.Text == "")
+            {
+                balSzam.Text = "0";
+            }
+        }
+
+
+
+
+
+        private void Muveletklikk(object sender, EventArgs e)
+        {
+
+            char muv = (sender as Button).Text[0];
+            if (History.Items.Count > 0)
+            {
+                string utolso = History.Items[History.Items.Count - 1].ToString();
+                char utut = utolso[utolso.Length - 1];
+                if (utut == '+' || utut == '-' || utut == '*' || utut == '/')
+                {
+                    jobb = int.Parse(balSzam.Text);
+                    int eredm = 0;
+                    bool hiba = false;
+                    switch (utut)
+                    {
+                        case '+':
+                            eredm = bal + jobb;
+                            break;
+
+                        case '-':
+                            eredm = bal -jobb;
+                            break;
+
+                        case '*':
+                            eredm = bal * jobb;
+                            break;
+                        case '/':
+                            if (jobb != 0)
+                            {
+                                eredm = bal / jobb;
+                            }
+                            else
+                            {
+                                MessageBox.Show("Osztás nullával");
+                                hiba = true;
+                            }
+                            break;
+                    }
+                    if (!hiba)
+                    {
+                        History.Items[History.Items.Count - 1] += eredm.ToString();
+                        if (muv == '=')
+                        {
+                            balSzam.Text = "0";
+                        }
+                        else {
+                            bal = eredm;
+                            History.Items.Add(eredm.ToString() + muv.ToString());
+                        }
+                    }     
+                }
+                else
+                {
+                    bal = int.Parse(balSzam.Text);
+                    History.Items.Add(bal.ToString()+ muv.ToString());
+                }
+
+            }
+            else
+            {
+                bal = int.Parse(balSzam.Text);
+                History.Items.Add(bal.ToString() + muv.ToString());
+            }
+            balSzam.Text = "0";
+
+        }
+    }
+}
+
