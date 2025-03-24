25e54731abfd4b380f90dc4b1ef611a6f3e8bcbf
Fekete Balázs <fekete.balazs@blathy.info>
2025-03-24 13:09:05 +0100

    Ez csak a közös osztó

---
+++ Fekete/Rendezesek.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Rendezesek
+{
+    static void Main()
+    {
+        Random rnd = new ();
+        int szam = rnd.Next(150,360);
+        System.Console.WriteLine("Erre a számra gondoltam: " +szam);
+        int masik = rnd.Next(150,360);
+        System.Console.WriteLine("És másik szám: " +masik);
+
+        System.Console.Write("A két szám közös osztói: "); 
+
+
+        for ( int i = 1; i< Math.Min(szam, masik); i++)
+        {
+                if ( szam % i == 0 && masik % i == 0){
+                    System.Console.Write(i + " ");
+                }
+        }
+    }
+
+}
+
+
